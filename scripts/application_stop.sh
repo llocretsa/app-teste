2ccaef63898c39b6b924df543280da9f5ac28ae8
llocretsa <parny.paul@gmail.com>
2022-01-14 08:05:33 -0300

    fix: fix teste

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#Stopping existing node servers
-# echo "Stopping any existing node servers"
-# sudo pkill node
-
 STATUS="$(sudo systemctl is-active node)"
 if [ "${STATUS}" = "active" ]; then
     echo sudo pkill node
@@ -11,3 +7,7 @@ else
     echo " Service not running... so exiting "
     exit 1
 fi
+
+#Stopping existing node servers
+echo "Stopping any existing node servers"
+# sudo pkill node
