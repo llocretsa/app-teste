85dd07a99772fd3aa7c86d3dd76ff4e7908c17a5
llocretsa <parny.paul@gmail.com>
2022-01-13 21:36:24 -0300

    fix: permição para execução

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+sudo nvm install 16
 
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/app"
@@ -11,5 +11,5 @@ if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+  sudo mkdir ${DIR}
 fi
